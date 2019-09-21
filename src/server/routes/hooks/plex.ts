e9064f4af26a968334d2e9859dc5f98491e98862
John Clayton <john@codemonkeylabs.com>
2019-09-21 11:19:32 -0600

    More robust handling of Plex webhook

--- src/server/routes/hooks/plex.ts
+++ src/server/routes/hooks/plex.ts
@@ -1,13 +1,25 @@
-import { Request, Response } from 'express';
-import multer from 'multer';
+import { NextFunction, Request, Response } from 'express';
+import { default as multer } from 'multer';
 
 const upload = multer();
 
-export default [upload.single('thumb'), function (req: Request, res: Response) {
+export default [
+  upload.single('thumb'),
+  function (req: Request, res: Response) {
 
-  const { body } = req;
-  const payload = JSON.parse(body.payload);
-  req.log.info({ payload }, 'Webhook received');
+    const { body } = req;
+    let { payload } = body;
+    if (!payload) {
+      return res.sendStatus(200);
+    }
+    payload = JSON.parse(payload);
 
-  res.sendStatus(200);
-}]
+    req.log.info({ payload }, 'Webhook received');
+
+    res.sendStatus(200);
+  },
+  function (err: any, req: Request, res: Response, next: NextFunction) {
+
+    req.log.error(err);
+    return res.sendStatus(200);
+  }];
