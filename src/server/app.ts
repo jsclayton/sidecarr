bb20afd55ab366b4498417d8a164fb29f13d1ff1
John Clayton <john@codemonkeylabs.com>
2019-09-21 00:24:29 -0600

    Parse multipart form hooks

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,8 +2,10 @@ import { default as express } from 'express';
 import * as routes from './routes';
 import pino from 'express-pino-logger';
 import uuid from 'uuid';
+import multer from 'multer';
 
 const app = express();
+const upload = multer();
 
 app.use(pino({
   enabled: process.env.NODE_ENV !== 'test'
@@ -11,7 +13,7 @@ app.use(pino({
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.post('/hooks/plex', routes.hooks.log);
+app.post('/hooks/plex', upload.single('thumb'), routes.hooks.log);
 app.post('/hooks/radarr', routes.hooks.log);
 app.post('/hooks/sonarr', routes.hooks.log);
 
