f8cd2f3a831613601a7d13f78b089c249fcebc04
John Clayton <john@codemonkeylabs.com>
2019-09-24 22:40:57 -0600

    Post to Slack on Plex webhook

--- src/server/routes/hooks/plex.ts
+++ src/server/routes/hooks/plex.ts
@@ -1,20 +1,39 @@
 import { NextFunction, Request, Response } from 'express';
 import { default as multer } from 'multer';
+import { WebClient } from '@slack/web-api';
 
 const upload = multer();
 
+const slack = new WebClient(process.env.SLACK_TOKEN);
+
+interface PlexWebhook {
+  event: string,
+  Account: {
+    title: string
+  }
+}
+
 export default [
   upload.single('thumb'),
-  function (req: Request, res: Response) {
+  async function (req: Request, res: Response) {
 
     const { body } = req;
     let { payload } = body;
     if (!payload) {
       return res.sendStatus(200);
     }
-    payload = JSON.parse(payload);
+    const plexPayload: PlexWebhook =  payload = JSON.parse(payload);
 
     req.log.info({ payload }, 'Webhook received');
+
+    await slack.chat.postMessage({
+      channel: 'plex',
+      text: 'Received webhook from Plex',
+      attachments: [
+        { text: plexPayload.event },
+        { text: plexPayload.Account.title }
+      ]
+    })
 
     res.sendStatus(200);
   },
