a0acc3fb78eacfbbfd22de9d71fece6446d35279
John Clayton <john@codemonkeylabs.com>
2019-11-03 10:06:46 -0700

    Refactor webhook message out of the route

--- src/server/routes/hooks/plex.ts
+++ src/server/routes/hooks/plex.ts
@@ -1,86 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { default as multer } from 'multer';
 import asyncHandler from '../../../server/asyncHandler';
-import slack from '../../../services/slack';
+import { WebhookMessage } from '../../../services/slack';
 import { Payload } from '../../../plex/models/webhooks';
 
 const upload = multer();
-
-class Message {
-
-  [props: string]: unknown;
-
-  text: string = '';
-
-  private constructor(payload: Payload) {
-
-    this.blocks = [
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: this.formatTitle(payload),
-          verbatim: true
-        },
-      },
-      {
-        type: 'context',
-        elements: [
-          {
-            type: 'image',
-            image_url: payload.account.thumb,
-            alt_text: payload.account.title
-          },
-          {
-            type: 'mrkdwn',
-            text: `*${payload.account.title}* ${this.formatEvent(payload.event)} on *${payload.player.title}*`
-          }
-        ]
-      }
-    ]
-    this.username = payload.server.title
-  }
-
-  static fromPayload(payload: Payload) : Message | undefined {
-
-    if (payload.metadata.type === 'track') {
-      return;
-    }
-
-    return new Message(payload);
-  }
-
-  private formatEvent(event: string) {
-
-    switch (event) {
-      case 'media.play':
-        return 'is watching';
-      case 'media.stop':
-        return 'stopped'
-      case 'media.pause':
-        return 'paused';
-      case 'media.resume':
-        return 'resumed';
-      case 'media.scrobble':
-        return 'scrobbled';
-      default:
-        return event
-    }
-  }
-
-  private formatTitle(payload: Payload) : string {
-
-    const { metadata, server } = payload;
-    const title = metadata.type === 'episode' ? metadata.grandparentTitle : metadata.title;
-    const subtitle = metadata.type === 'episode' ? metadata.title : metadata.tagline;
-    return `*${title}*\n${subtitle}`;
-  }
-
-  async post(channel: string) {
-
-    await slack.chat.postMessage({ ...this, channel })
-  }
-}
 
 export default [
   upload.single('thumb'),
@@ -96,7 +20,7 @@ export default [
     req.log.info({ payload }, 'Webhook received');
 
     // Future hawtness: https://github.com/tc39/proposal-nullish-coalescing
-    const message = Message.fromPayload(payload);
+    const message = WebhookMessage.fromPayload(payload);
     if (message) {
       await message.post('plex');
     }
--- src/services/slack.ts
+++ src/services/slack.ts
@@ -1,3 +1,80 @@
 import { WebClient } from '@slack/web-api';
+import { Payload } from '../plex/models/webhooks';
 
-export default new WebClient(process.env.SLACK_TOKEN);
+const slack = new WebClient(process.env.SLACK_TOKEN);
+
+export class WebhookMessage {
+
+  [props: string]: unknown;
+
+  private constructor(payload: Payload) {
+
+    this.blocks = [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: this.formatTitle(payload),
+          verbatim: true
+        },
+      },
+      {
+        type: 'context',
+        elements: [
+          {
+            type: 'image',
+            image_url: payload.account.thumb,
+            alt_text: payload.account.title
+          },
+          {
+            type: 'mrkdwn',
+            text: `*${payload.account.title}* ${this.formatEvent(payload.event)} on *${payload.player.title}*`
+          }
+        ]
+      }
+    ]
+    this.username = payload.server.title
+  }
+
+  static fromPayload(payload: Payload): WebhookMessage | undefined {
+
+    if (payload.metadata.type === 'track') {
+      return;
+    }
+
+    return new WebhookMessage(payload);
+  }
+
+  private formatEvent(event: string) {
+
+    switch (event) {
+      case 'media.play':
+        return 'is watching';
+      case 'media.stop':
+        return 'stopped'
+      case 'media.pause':
+        return 'paused';
+      case 'media.resume':
+        return 'resumed';
+      case 'media.scrobble':
+        return 'scrobbled';
+      default:
+        return event
+    }
+  }
+
+  private formatTitle(payload: Payload): string {
+
+    const { metadata, server } = payload;
+    const title = metadata.type === 'episode' ? metadata.grandparentTitle : metadata.title;
+    const subtitle = metadata.type === 'episode' ? metadata.title : metadata.tagline;
+    return `*${title}*\n${subtitle}`;
+  }
+
+  async post(channel: string) {
+
+    await slack.chat.postMessage({ text: '', ...this, channel })
+  }
+}
+
+export default slack;
