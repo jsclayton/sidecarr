67b3a1c3934465240d2f28143fd57828e7885d1c
John Clayton <john@codemonkeylabs.com>
2020-06-26 15:55:13 -0600

    Fix datadog source

--- bin/exec-datadog.sh
+++ bin/exec-datadog.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-trap INT
-exec `echo "$@"` | pino-datadog -k $DD_KEY -d $DD_SOURCE -s plexbuddy | pino-pretty -c -t "SYS:yyyy-mm-dd HH:MM:ss" -i pid,hostname
+trap exit 0 INT
+exec `echo "$@"` | pino-datadog -k $DD_KEY -d $DD_SOURCE -s sidecarr | pino-pretty -c -t "SYS:yyyy-mm-dd HH:MM:ss" -i pid,hostname
--- bin/exec-pretty.sh
+++ bin/exec-pretty.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-trap INT
+trap exit 0 INT
 exec `echo "$@"` | pino-pretty -c -t "SYS:yyyy-mm-dd HH:MM:ss" -i pid,hostname
