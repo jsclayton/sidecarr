7b06b501c83fdf208aca93db03502f2725e0667b
John Clayton <john@codemonkeylabs.com>
2021-08-08 10:31:03 -0600

    Upgrade to TS 4

--- src/server/asyncHandler.ts
+++ src/server/asyncHandler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 
 export default function asyncHandler (func: RequestHandler) {
   return (req: Request, res: Response, next: NextFunction) => {
-    const result = func.call(undefined, req, res, next);
+    const result: any = func.call(undefined, req, res, next);
     return result && result.catch ? result.catch(next) : result;
   }
 }
